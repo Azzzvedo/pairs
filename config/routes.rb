c9cc1962b1797807c4ac7fef1854baac92483500
Luz Valdovinos <luz.valdovinos@gmail.com>
2022-03-01 17:53:10 +0000

    created profile edit  controller, view, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :pairing_sessions, only: %w[new create]
+  get "/myprofile", to: "profiles#show", as: :my_profile
+  patch "/myprofile", to: "profiles#update"
+  get "/myprofile/edit", to: "profiles#edit", as: :edit_my_profile
 end

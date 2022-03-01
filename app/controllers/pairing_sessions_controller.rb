c2ef517d49b3cec082ec00b6f839e7c6abed4284
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-03-01 18:26:15 +0000

    get map working again

--- app/controllers/pairing_sessions_controller.rb
+++ app/controllers/pairing_sessions_controller.rb
@@ -7,6 +7,13 @@ class PairingSessionsController < ApplicationController
 
   def new
     @pairing_session = PairingSession.new
+    @pairing_sessions = PairingSession.all
+    @markers = @pairing_sessions.geocoded.map do |pairing_session|
+      {
+        lat: pairing_session.latitude,
+        lng: pairing_session.longitude
+      }
+    end
   end
 
   # def update

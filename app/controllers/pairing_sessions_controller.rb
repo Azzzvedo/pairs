39bced424682e860a2887c02a4e5a99833e10a9d
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-03-01 21:45:02 +0000

    form changes

--- app/controllers/pairing_sessions_controller.rb
+++ app/controllers/pairing_sessions_controller.rb
@@ -1,12 +1,6 @@
 class PairingSessionsController < ApplicationController
 
-
-  # def index
-  #   @pairing_sessions = PairingSession.all
-  # end
-
-  def new
-    @pairing_session = PairingSession.new
+  def index
     @pairing_sessions = PairingSession.all
     @markers = @pairing_sessions.geocoded.map do |pairing_session|
       {
@@ -16,6 +10,24 @@ class PairingSessionsController < ApplicationController
     end
   end
 
-  # def update
-  # end
+  def new
+    @pairing_session = PairingSession.new
+
+  end
+
+  def create
+    @pairing_session = PairingSession.new(pairing_session_params)
+    @pairing_session.user = current_user
+    if @pairing_session.save
+      redirect_to pairing_sessions_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def pairing_session_params
+    params.require(:pairing_session). permit(:activity, :address, :time, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :pairing_sessions, only: %w[new create]
+  resources :pairing_sessions, only: %i[index new create]
   get "/myprofile", to: "profiles#show", as: :my_profile
   patch "/myprofile", to: "profiles#update"
   get "/myprofile/edit", to: "profiles#edit", as: :edit_my_profile

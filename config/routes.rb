842e688d39ff1940831c1817f0599ecc1c853fe3
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-03-01 17:52:40 +0000

    form and map added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :pairing_sessions, only: %w[new create]
+  resources :pairing_sessions, only: %i[new create]
 end

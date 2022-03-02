fed0aefe16d0ddbc6882172334a56d3fff9884da
Arabella Stephenson <arabella.stephenson@gmail.com>
2022-03-02 16:26:43 +0100

    markers

--- app/controllers/pairing_sessions_controller.rb
+++ app/controllers/pairing_sessions_controller.rb
@@ -1,18 +1,18 @@
 class PairingSessionsController < ApplicationController
-
   def index
     @pairing_sessions = PairingSession.all
     @markers = @pairing_sessions.geocoded.map do |pairing_session|
       {
         lat: pairing_session.latitude,
-        lng: pairing_session.longitude
+        lng: pairing_session.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { pairing_session: pairing_session }),
+        image_url: helpers.asset_url("fire-flame.svg")
       }
     end
   end
 
   def new
     @pairing_session = PairingSession.new
-
   end
 
   def create

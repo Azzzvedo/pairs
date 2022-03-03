8b7160883fd07057357a2ed5899793d2a5b0a06a
Joshua Walker <josh.wm.walker@gmail.com>
2022-03-03 10:25:47 +0000

    Some, but no all bugs fixed - still got The Big One

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -1,6 +1,41 @@
-// Action Cable provides the framework to deal with WebSockets in Rails.
-// You can generate new channels where WebSocket features live using the `bin/rails generate channel` command.
-
 import { createConsumer } from "@rails/actioncable"
 
 export default createConsumer()
+
+import consumer from "./consumer";
+const insertIntoDOM = (messageHTML, currentUserId, messages) => {
+  const message = document.createElement('div')
+  message.innerHTML = messageHTML;
+
+  if (message.dataset.senderId === currentUserId) {
+
+    message.firstChild.classList.add('message  current');
+  } else {
+
+    message.firstChild.classList.add('message  other');
+  }
+
+  messages.insertAdjacentElement('beforeend', message);
+}
+
+const Chat = () => {
+
+  const messages = document.getElementsByClassName('messages');
+
+  if (messages) {
+
+    const chatId = messages.dataset.chatId;
+    const currentUserId = messages.dataset.currentUserId;
+
+
+    consumer.subscriptions.create(
+      { channel: 'ChatChannel', id: chatId },
+      {
+        received(messageHTML) {
+          console.log(messageHTML)
+          insertIntoDOM(messageHTML, currentUserId, messages);
+        }
+      }
+    )
+  }
+}

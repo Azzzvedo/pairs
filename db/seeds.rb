acaaa61503e91d585aa83d2a6ee051ff869da9b2
Arabella Stephenson <arabella.stephenson@gmail.com>
2022-03-03 17:59:06 +0100

    some elements of styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,30 +15,30 @@ ACTIVITIES = %w[climbing jai\ alai fencing]
 User.destroy_all
 PairingSession.destroy_all
 
-5.times do
-user = User.new(
-  email: Faker::Internet.email,
-  password: "123456",
-  address: Faker::Address.street_address,
-  first_name: Faker::Name.first_name,
-  last_name: Faker::Name.last_name,
-  gender: Faker::Gender.binary_type,
-  bio: Faker::Twitter.status,
-  birth_date: Faker::Date.birthday(min_age: 18, max_age: 65),
-  competitiveness: Faker::Number.within(range: 1..10)
-)
+  5.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    password: "123456",
+    address: Faker::Address.street_address,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    gender: Faker::Gender.binary_type,
+    bio: Faker::Twitter.status,
+    birth_date: Faker::Date.birthday(min_age: 18, max_age: 65),
+    competitiveness: Faker::Number.within(range: 1..10)
+  )
 user.save!
 end
 
 i = 0
-5.times do
-pairing_session = PairingSession.new(
-  activity: ACTIVITIES.sample,
-  address: LOCATIONS[i],
-  description: Faker::Twitter.status[:text],
-  time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
-  user_id: User.all.sample.id
-)
-i = i +1
+  5.times do
+    pairing_session = PairingSession.new(
+      activity: ACTIVITIES.sample,
+      address: LOCATIONS[i],
+      description: Faker::Twitter.status[:text],
+      time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+      user_id: User.all.sample.id
+    )
+  i = i +1
 pairing_session.save!
 end

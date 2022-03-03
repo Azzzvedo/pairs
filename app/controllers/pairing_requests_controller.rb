e3ae445d2dfe59d6d336d0fd1c15c0ecc19831ea
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-03-03 17:11:55 +0000

    pairing requests working

--- app/controllers/pairing_requests_controller.rb
+++ app/controllers/pairing_requests_controller.rb
@@ -1,9 +1,26 @@
 class PairingRequestsController < ApplicationController
   def create
-    # @pairing_request = PairingRequest.new(user: current_user)
-    # @pairing_request.pairing_session =
+    @pairing_request = PairingRequest.new(pairing_request_params)
+    @pairing_session = PairingSession.find(params[:pairing_session_id])
+    @pairing_request.pairing_session = @pairing_session
+    @pairing_request.user = current_user
+    if @pairing_request.save
+      redirect_to pairing_request_path(@pairing_request)
+    else
+      render 'pairing_session/index'
+    end
+  end
+
+  def show
+    @pairing_request = PairingRequest.find(params[:id])
   end
 
   def destroy
+  end
+
+  private
+
+  def pairing_request_params
+    params.require(:pairing_request).permit(:introduction)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'pairing_requests/create'
-  get 'pairing_requests/destroy'
   devise_for :users
   root to: 'pages#home'
   resources :chats, only: [:show, :create, :index] do
@@ -11,7 +9,8 @@ Rails.application.routes.draw do
   resources :pairing_sessions, only: %i[index new create] do
     resources :pairing_requests, only: %i[index create]
   end
-    resources :pairing_sessions, only: %w[new create]
+  resources :pairing_requests, only: %i[index show]
+
   get "profile/edit", to: "pages#edit", as: :edit_my_profile
   get "profile/:id", to: "pages#profile", as: :my_profile
   patch "profile/:id", to: "pages#update"

dfeb5bbf54f479b56f807d1a8b2198d9880234fa
Joshua Walker <josh.wm.walker@gmail.com>
2022-03-03 17:17:40 +0000

    Request index implimenting

--- app/controllers/pairing_requests_controller.rb
+++ app/controllers/pairing_requests_controller.rb
@@ -1,4 +1,8 @@
 class PairingRequestsController < ApplicationController
+  def index
+    @pairing_sessions = current_user.pairing_sessions
+  end
+
   def create
     # @pairing_request = PairingRequest.new(user: current_user)
     # @pairing_request.pairing_session =
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,11 @@ Rails.application.routes.draw do
   resources :chats, only: [:show, :create, :index] do
     resources :messages, only: :create
   end
-
+  resources :pairing_requests, only: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :pairing_sessions, only: %i[index new create] do
-    resources :pairing_requests, only: %i[index create]
+    resources :pairing_requests, only: %i[create]
   end
-    resources :pairing_sessions, only: %w[new create]
   get "profile/edit", to: "pages#edit", as: :edit_my_profile
   get "profile/:id", to: "pages#profile", as: :my_profile
   patch "profile/:id", to: "pages#update"

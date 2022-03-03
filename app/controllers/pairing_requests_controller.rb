ddda601511ebe30496e9132b4c3e4eb93315b1ae
Joshua Walker <josh.wm.walker@gmail.com>
2022-03-03 19:22:16 +0000

    Few tweaks here and there

--- app/controllers/pairing_requests_controller.rb
+++ app/controllers/pairing_requests_controller.rb
@@ -3,6 +3,11 @@ class PairingRequestsController < ApplicationController
     @pairing_sessions = current_user.pairing_sessions
   end
 
+  def create_chat
+    @pairing_request = PairingRequest.find(params[:id])
+    Chat.new(sender_id: current_user.id, recipient_id: pairing_request.user_id) if @pairing_request.approved == true
+  end
+
   def create
     @pairing_request = PairingRequest.new(pairing_request_params)
     @pairing_session = PairingSession.find(params[:pairing_session_id])

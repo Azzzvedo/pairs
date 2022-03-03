9fe4b9c167e97bde209d36fb058227e1c62ff2a2
Arabella Stephenson <arabella.stephenson@gmail.com>
2022-03-03 13:59:39 +0100

    basic seed info

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require "open-uri"
+require "nokogiri"
+require "pry"
+
+LOCATIONS = ["Hyde Park", "Regents Park", "Primrose Hill", "Richmond Park", "Victoria Park"]
+ACTIVITIES = %w[climbing jai\ alai fencing]
+User.destroy_all
+PairingSession.destroy_all
+
+5.times do
+user = User.new(
+  email: Faker::Internet.email,
+  password: "123456",
+  address: Faker::Address.street_address,
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  gender: Faker::Gender.binary_type,
+  bio: Faker::Twitter.status,
+  birth_date: Faker::Date.birthday(min_age: 18, max_age: 65),
+  competitiveness: Faker::Number.within(range: 1..10)
+)
+user.save!
+end
+
+i = 0
+5.times do
+pairing_session = PairingSession.new(
+  activity: ACTIVITIES.sample,
+  address: LOCATIONS[i],
+  description: Faker::Twitter.status[:text],
+  time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+  user_id: User.all.sample.id
+)
+i = i +1
+pairing_session.save!
+end

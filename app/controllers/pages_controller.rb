d8ee9cfab254a204f69b64fdf7fb7936563fb48c
Joshua Walker <josh.wm.walker@gmail.com>
2022-03-07 12:41:31 +0000

    Yet more chat fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home, raise: false
 
-  def home
-
-  end
+  def home; end
 
   def profile
     @user = User.find(params[:id])

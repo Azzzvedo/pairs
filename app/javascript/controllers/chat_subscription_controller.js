1110cb53ef6b91b6950e93e7c61c8c8892270a7d
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-03-11 09:35:58 +0000

    chat fixed by louis

--- app/javascript/controllers/chat_subscription_controller.js
+++ app/javascript/controllers/chat_subscription_controller.js
@@ -42,12 +42,14 @@ export default class extends Controller {
     console.log(data)
     this.messagesTarget.insertAdjacentHTML("beforeend", data)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
-    const senderId = msg.dataset.senderId
+    const senderId = parseInt(data.split("data-sender-id=")[1].split(">"))
+    console.log(`Sender ->  ${senderId}`)
     const currentUserId = this.element.dataset.currentUserId
+    console.log(typeof currentUserId)
+    this.styleMessages()
     if (currentUserId == senderId) {
       this.formTarget.reset()
     }
     this.formTarget.scrollIntoView()
-    this.styleMessages()
   }
 }
